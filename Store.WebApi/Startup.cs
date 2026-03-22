47ba87ab69f16b59521278ec02deba45f95fa9b5
Matin Karuti <matin.ka@hotmail.com>
2026-03-22 18:37:46 +0100

    Enable http2 for Store Server

--- Store.WebApi/Startup.cs
+++ Store.WebApi/Startup.cs
@@ -1,10 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using Domain.StoreSystem;
 using Domain.StoreSystem.repository;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Store.Grpc.Services;
 using Store.Integration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Kestrel für HTTP/2 konfigurieren
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5131, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
+
+    options.ListenAnyIP(7138, listenOptions =>
+    {
+        listenOptions.UseHttps();
+        listenOptions.Protocols = HttpProtocols.Http2; // Nur HTTP/2 auf HTTPS
+    });
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
